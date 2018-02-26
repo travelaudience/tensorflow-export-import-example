06d556288d844c13ee726e5f9fffad1a16566ccb
Wolfgang Steitz <w.steitz@travelaudience.com>
2018-02-26 18:46:24 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Training tensorflow models in Python. Serving with Golang.
+
+Example code to setup, train and export a basic tensorflow model in Python and then import that model in Go and start predicting.
+
+
+## Usage
+
+tensorflow needs to be installed in your Python and Go environment. For Python `pip install -r requirements` is enough. For Go, please refer to https://www.tensorflow.org/install/install_go
+
+To run the example code, just do:
+
+
+     python example.py
+     go run example.go
---
+++ example.py
@@ -0,0 +1,73 @@
+import tensorflow as tf
+import shutil
+
+
+# setup feature columns
+domain = tf.feature_column.categorical_column_with_hash_bucket(
+ "domain", 100000, dtype=tf.string)
+hour = tf.feature_column.categorical_column_with_identity("hour", 24)
+device_type = tf.feature_column.categorical_column_with_vocabulary_list(
+ "device_type", vocabulary_list=["desktop", "mobile", "tablet"],
+ default_value=0)
+feature_columns = [domain, hour, device_type]
+
+
+# actual model setup
+ftrl = tf.train.FtrlOptimizer(
+    learning_rate=0.1,
+    learning_rate_power=-0.5,
+    l1_regularization_strength=0.001,
+    l2_regularization_strength=0.0
+    )
+
+model_dir = "."
+estimator = tf.estimator.LinearClassifier(
+    feature_columns=feature_columns,
+    optimizer=ftrl,
+    model_dir=model_dir
+    )
+
+
+def input_fn(paths):
+    """ model input function """
+
+    names = ["domain", "hour", "device_type", "is_click"]
+    record_defaults = [[""], [0], ["desktop"], [0]]
+
+    def _parse_csv(rows_string_tensor):
+        columns = tf.decode_csv(rows_string_tensor, record_defaults)
+        features = dict(zip(names, columns[:-1]))
+        labels = columns[-1]
+        return features, labels
+
+    def _input_fn():
+        dataset = tf.data.TextLineDataset(paths)
+        dataset = dataset.map(_parse_csv)
+        dataset = dataset.batch(100)
+        iterator = dataset.make_one_shot_iterator()
+        features, labels = iterator.get_next()
+        return features, labels
+
+    return _input_fn
+
+
+# Train the model.
+paths = ["example.csv"]
+estimator.train(input_fn=input_fn(paths), steps=None)
+
+
+# Export our model
+feature_spec = tf.feature_column.make_parse_example_spec(feature_columns)
+columns = [('hour', tf.int64),
+           ('domain', tf.string),
+           ('device_type', tf.string)]
+feature_placeholders = {
+ name: tf.placeholder(dtype, [1], name=name + "_placeholder")
+ for name, dtype in columns
+}
+export_input_fn = tf.estimator.export.build_raw_serving_input_receiver_fn(
+    feature_placeholders)
+path = estimator.export_savedmodel(model_dir, export_input_fn)
+
+# rename export directory
+shutil.move(path, "EXPORT")
