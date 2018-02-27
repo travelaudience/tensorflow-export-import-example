5cc54d56375a6d45e7d3eae1db9ae314dd14821d
Wolfgang Steitz <w.steitz@travelaudience.com>
2018-02-27 15:48:38 +0100

    minor cleanup of Go code

--- example.go
+++ example.go
@@ -1,32 +1,32 @@
 package main
 
 import (
-    tf "github.com/tensorflow/tensorflow/tensorflow/go"
-    tg "github.com/galeone/tfgo"
-    "fmt"
+	"fmt"
+
+	tg "github.com/galeone/tfgo"
+	tf "github.com/tensorflow/tensorflow/tensorflow/go"
 )
 
 func main() {
 
-  // Import model.
+	// Import model.
+	model := tg.LoadModel("EXPORT", []string{"serve"}, &tf.SessionOptions{})
 
-  model := tg.LoadModel("EXPORT", []string{"serve"}, &tf.SessionOptions{})
+	// Create input tensors.
+	deviceInput, _ := tf.NewTensor([]string{"desktop"})
+	hourInput, _ := tf.NewTensor([]int64{19})
+	domainInput, _ := tf.NewTensor([]string{"foo.com"})
 
-  // Create input tensors.
-  deviceInput, _ := tf.NewTensor([1]string{"desktop"})
-  hourInput, _ := tf.NewTensor([1]int64{19})
-  domainInput, _ := tf.NewTensor([1]string{"foo.com"})
-
-  // Predict.
-  results := model.Exec(
-    []tf.Output{
-      model.Op("linear/head/predictions/probabilities", 0),
-    }, map[tf.Output]*tf.Tensor{
-      model.Op("device_type_placeholder", 0): deviceInput,
-      model.Op("hour_placeholder", 0): hourInput,
-      model.Op("domain_placeholder", 0): domainInput,
-    },
-  )
-  predictions := results[0].Value().([][]float32)
-  fmt.Println(predictions)
+	// Predict.
+	results := model.Exec(
+		[]tf.Output{
+			model.Op("linear/head/predictions/probabilities", 0),
+		}, map[tf.Output]*tf.Tensor{
+			model.Op("device_type_placeholder", 0): deviceInput,
+			model.Op("hour_placeholder", 0):        hourInput,
+			model.Op("domain_placeholder", 0):      domainInput,
+		},
+	)
+	predictions := results[0].Value().([][]float32)
+	fmt.Println(predictions)
 }
